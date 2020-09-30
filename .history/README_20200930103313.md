d43f9410277453f9da5c160b5e179fbc9678783d
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2020-09-30 10:37:02 +0200

    upload file

---
+++ .history/README_20200930103313.md
@@ -0,0 +1,2 @@
+# proportionsJS
+Scaling dynamically HTML block, images and whatsoever else
---
+++ src/proportions.js
@@ -0,0 +1,54 @@
+const PROPORTIONS = document.querySelectorAll("[data-proportions]");
+let proportionTypes = [];
+if (PROPORTIONS.length > 0) {
+  class Proportions {
+    constructor(proportions) {
+      this.p = proportions.split(",")[1].split("-"); // Name and proportions
+      this.h = 0; // height
+      this.w = 0; // width
+      this.timeout;
+      this.d = 0; // latency
+
+      this.resize = () => {
+        this.e = document.querySelectorAll(
+          "[data-proportions='" + proportions + "']"
+        ); // Elementy tego samego typu
+        this.w = this.e[0].offsetWidth;
+        this.h = (this.w / this.p[0]) * this.p[1];
+        for (const el of this.e) {
+          el.style.height = this.h + "px";
+        }
+      };
+
+      this.init = () => {
+        window.clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          this.resize();
+        }, this.d);
+      };
+
+      this.init();
+      window.addEventListener("load", () => {
+        this.init();
+      });
+
+      window.addEventListener("resize", () => {
+        this.init();
+      });
+    }
+  }
+
+  const initProportions = () => {
+    for (const el of PROPORTIONS) {
+      const proportionEl = el.dataset.proportions;
+      if (!proportionTypes.includes(proportionEl));
+      proportionTypes.push(proportionEl);
+    }
+
+    for (let el of proportionTypes) {
+      el = new Proportions(el);
+    }
+  };
+
+  initProportions();
+}
